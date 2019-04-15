9cb63346dd09f02dff4c35b618730565fd7cadf2
Your Name <lirannh@gmail.com>
2019-04-15 13:47:42 +0300

    add method in chat

--- app.py
+++ app.py
@@ -26,7 +26,6 @@ def SaveScoreToDatabase(How_I_feel,How_I_Sleep,Do_I_Feel_depressed,Do_I_Feel_anx
     ,Do_I_Feel_depressed,Do_I_Feel_anxiety,Do_I_Feel_Fear,Have_appetite,myscore,_Date,_User_ID)
     
 
-
 def ConvertToDateString(item : "tuple (Date,Score)"):
     unix_date = item[0]
     score = item[1]
@@ -34,9 +33,6 @@ def ConvertToDateString(item : "tuple (Date,Score)"):
     local_time = time.strftime("%Y-%m-%d %H:%M:%S", local_time) 
 
     return (local_time,score)
-
-
-
 
 
 def GetScoreUserFromDatabase(user_id : "User id for get points"):
@@ -178,6 +174,7 @@ def login():
 
         User_exist , UserInformation = valid_login(usernameid,password)
 
+        print(UserInformation)
        
         session["userdata"] = list(UserInformation) ## save user information between pages
 
@@ -190,11 +187,11 @@ def login():
     else:
         return render_template("index.html")
 
-def GetMediceneFromTable(medicineId : "Medicine_id"):
+def GetMediceneFromTable(medicineId : "Medicine_id",selectedList : list):
     MedicQuery = {
     ConstDic.TABLENAME : "Medicine",
 
-    ConstDic.SELECTED : ["Medicine_Name","Description"],
+    ConstDic.SELECTED : selectedList,
 
     ConstDic.FILTER : {
 
@@ -212,7 +209,7 @@ def GetMediceneFromTable(medicineId : "Medicine_id"):
 
 
 
-def GetMedicationWithDesc():
+def GetIDMedication():
     UserInformation = session.pop('userdata', None)
     session["userdata"] = list(UserInformation) ## save user information between pages
     
@@ -235,29 +232,285 @@ def GetMedicationWithDesc():
 
     result_FromMedicine_User = db.Select(MedicUseQuery)
 
-
-    mylist_of_medic = []
-    for Mediceine_id in result_FromMedicine_User:
-        result_from_Medicene_desc = GetMediceneFromTable(Mediceine_id[0])
-        mylist_of_medic.append(result_from_Medicene_desc)
-
-    return mylist_of_medic
+    return result_FromMedicine_User
 
 
+def GetDiagnosisNameWithDesc():
+    UserInformation = session.pop('userdata', None)
+    session["userdata"] = list(UserInformation) ## save user information between pages
+    User_id = UserInformation[0]
+    ## get user id from cookies
+
+    ## step 1 , get diagnosis id from patient table
+    DiagnosisQuery = {
+
+    ConstDic.TABLENAME : "Patient",
+
+    ConstDic.SELECTED : ["Diagnosis_ID"],
+
+    ConstDic.FILTER : {
+
+            "User_ID" :User_id
+    },
+
+    ConstDic.TYPEFETCH : FetchType.FETCH_ONE
+
+    }
+
+    idDiagnosis = db.Select(DiagnosisQuery)
+
+
+    ## step two , get name and description from diagnosis by id diagnosis
+
+    DiagnosisTableQuery = {
+
+    ConstDic.TABLENAME : "Diagnosis",
+
+    ConstDic.SELECTED : ["Diagnosis_Name","Description"],
+
+    ConstDic.FILTER : {
+
+            "Diagnosis_ID" : idDiagnosis[0]
+    },
+
+    ConstDic.TYPEFETCH : FetchType.FETCH_ONE
+
+    }
+
+    result = db.Select(DiagnosisTableQuery)
+
+    return result
+
+
+
+
+def CheckIfKeywordsInSentence(sentence : "data user input",keywords : list):
+    return any(key in sentence for key in keywords)
+    
+
+def GetMedicationWithDesc(idMedics : list):
+    result = []
+    for medID in idMedics:
+        result.append(GetMediceneFromTable(medID[0],["Medicine_Name","Description","Additional_guidelines"]))
+    return result
+
+
+def GetMedicWithSideEffect(idMeds:list):
+    result = []
+    for medID in idMeds:
+        result.append(GetMediceneFromTable(medID[0],["Medicine_Name","Side_Effects"]))
+    return result
+
+def GetMedicWithDosage():
+    UserInformation = session.pop('userdata', None)
+    session["userdata"] = list(UserInformation) ## save user information between pages
+    
+    User_id = UserInformation[0]
+
+    MedicUseQuery = {
+
+    ConstDic.TABLENAME : "Medicine_Use",
+
+    ConstDic.SELECTED : ["Dosage","Number_of_dose","Type of taking"],
+
+    ConstDic.FILTER : {
+
+            "User_ID" :User_id
+    },
+
+    ConstDic.TYPEFETCH : FetchType.FETCH_ALL
+
+    }
+
+    dosageInformationFromDB = db.Select(MedicUseQuery)
+
+    return dosageInformationFromDB
+
+
+def DepartmentDesc():
+    UserInformation = session.pop('userdata', None)
+    session["userdata"] = list(UserInformation) ## save user information between pages
+    User_id = UserInformation[0]
+    ## get user id from cookies
+
+    ## step 1 , 
+    DepartmentDescQuery = {
+
+    ConstDic.TABLENAME : "Patient",
+
+    ConstDic.SELECTED : ["Department_Name"],
+
+    ConstDic.FILTER : {
+
+            "User_ID" :User_id
+    },
+
+    ConstDic.TYPEFETCH : FetchType.FETCH_ONE
+
+    }
+
+    id_department = db.Select(DepartmentDescQuery)
+
+
+    ## step two , get name department 
+
+    departmentNameAndTypeQuery = {
+
+    ConstDic.TABLENAME : "Department",
+
+    ConstDic.SELECTED : ["Department_Name","Department_Type"],
+
+    ConstDic.FILTER : {
+
+            "Department_Name" : id_department[0]
+    },
+
+    ConstDic.TYPEFETCH : FetchType.FETCH_ONE
+
+    }
+
+    result = db.Select(departmentNameAndTypeQuery)
+
+    return result
+
+def rightsUrl():
+    return '<a href="https://hospitals.clalit.co.il/geha/he/patient/Pages/rights.aspx">Right Page </a>'
+
+def DefaultMessage():
+    return """ I can not help you in this field. I can help you in the following fields: medication, diagnosis, department, hospitalization type, personal care and rights"""
 
 
 def Parser(dataComing : "input from user"):
-    if("Medication" in dataComing or "pill" in dataComing or "Medicines" in dataComing):
-        return GetMedicationWithDesc()
+    dataComing = dataComing.lower()
+
+    mykeywordsDic = {
+
+        "med" : {
+
+            "keywords" : ["pill","medication", "medicines","drug","drugs"],
+            "sideeffects" : ["side effects","side effect"],
+            "dosage" : ["dosage"]
+        },
+        "diagnosis" : {
+            "keywords" : ["diagnosis", "prognosis", "diagnosis"]
+        },
+        "hospitalization" : {
+                "keywords" : ["hospitalization"]
+
+        },
+        "therapist" : {
+             "keywords" : ["therapist", "personal therapist"]
+        },
+        "department" : {
+            "keywords" : ["class", "department"]
+        },
+        "rights" : {
+            "keywords" : ["rights", "privilege"]
+        }
+    }
+
+    if(CheckIfKeywordsInSentence(dataComing,mykeywordsDic["med"]["keywords"])):
+        idMeds = GetIDMedication()
+
+        if(CheckIfKeywordsInSentence(dataComing,mykeywordsDic["med"]["sideeffects"])):
+            return GetMedicWithSideEffect(idMeds)
+            # return name + side effect
+        elif(CheckIfKeywordsInSentence(dataComing,mykeywordsDic["med"]["dosage"])):
+            dosageInformation =  GetMedicWithDosage()
+            namesMedic = list(zip(*GetMedicationWithDesc(idMeds)))[0]
+            mytext = ""
+            for name,information in zip(namesMedic,dosageInformation):
+                mytext += "<strong> Medication name : </strong>" + name + "<strong>  Dosage : </strong> "+ \
+                information[0] + "," + information[1]+" <strong> Type of taking :   </strong> "+ information[2]
+
+
+            return mytext
+            # return name + dosage
+        else:
+            listofTuples = GetMedicationWithDesc(idMeds)
+            mytextparser = str(list(map(lambda item : "<strong> Medication name : </strong>  " + item[0] + "<strong> Description: </strong> " + item[1] +" <strong> Addication guildline: </strong> " +item[2] ,listofTuples)))
+            mytextparser = mytextparser[2:-2]
+            mytextparser = mytextparser.replace("\r","")
+            mytextparser = mytextparser.replace("\n","")
+            mytextparser = mytextparser.replace("\r\n","")
+
+            print(mytextparser)
+            return mytextparser
+
+    elif(CheckIfKeywordsInSentence(dataComing,mykeywordsDic["diagnosis"]["keywords"])):
+        return GetDiagnosisNameWithDesc()
+
+    elif(CheckIfKeywordsInSentence(dataComing,mykeywordsDic["hospitalization"]["keywords"])):
+        pass # To Do after Or created table
+    elif(CheckIfKeywordsInSentence(dataComing,mykeywordsDic["therapist"]["keywords"])):
+        return TherapistDesc()
+    elif(CheckIfKeywordsInSentence(dataComing,mykeywordsDic["department"]["keywords"])):
+        return DepartmentDesc()
+    elif(CheckIfKeywordsInSentence(dataComing,mykeywordsDic["rights"]["keywords"])):
+        return rightsUrl()
+    else :
+        return DefaultMessage()
+        
+    
+
+    
+
+def TherapistDesc():
+    UserInformation = session.pop('userdata', None)
+    session["userdata"] = list(UserInformation) ## save user information between pages
+    User_id = UserInformation[0]
+    ## get user id from cookies
+
+    ## step 1 , get personal thrapist id from patient table
+    DiagnosisQuery = {
+
+    ConstDic.TABLENAME : "Patient",
+
+    ConstDic.SELECTED : ["Personal_therapist"],
+
+    ConstDic.FILTER : {
+
+            "User_ID" :User_id
+    },
+
+    ConstDic.TYPEFETCH : FetchType.FETCH_ONE
+
+    }
+
+    idthrapist = db.Select(DiagnosisQuery)
+
+
+    ## step two , get name and description from diagnosis by id diagnosis
+
+    StaffMemberTableQuery = {
+
+    ConstDic.TABLENAME : "Staff_Member",
+
+    ConstDic.SELECTED : ["Staff_Member_Name","Profession"],
+
+    ConstDic.FILTER : {
+
+            "Staff_Member_ID" : idthrapist[0]
+    },
+
+    ConstDic.TYPEFETCH : FetchType.FETCH_ONE
+
+    }
+
+    result = db.Select(StaffMemberTableQuery)
+
+    return result
+
+
+
 
 
 @app.route("/chatbot" ,methods =["POST"])
 def chatbot():
     dataComing = request.form["data"]
     
-    text = str(list(Parser(dataComing)))
+    return Parser(dataComing)
 
-    return text
 
 if __name__ == '__main__':
     app.run(debug=True)
