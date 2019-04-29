f25bdd18042bb4864e7183129632e43ac4fe6717
Your Name <lirannh@gmail.com>
2019-04-29 13:31:44 +0300

    remove setting

--- setting.py
+++ setting.py
@@ -1,8 +1,8 @@
 class Setting:
     def __init__(self, *args, **kwargs):
         self.setting = {'drivername': 'mysql',
-               'username': "lWphITcQFv",
-               'password': "yAcWZJk22z",
+               'username': "XXX",
+               'password': "XXXX",
                'host': "remotemysql.com",
                'port': 3306
-               ,'database':"lWphITcQFv"}
+               ,'database':"XXX"}
